09b27613942bb5bfe06f736bc01691018ba8ccfe
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-12-14 07:42:49 +0100

    Hilfsfunktionen

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 
 // Kommentiere hier (b)
 // 
-const svg = document.querySelector("#svg-field")
 const circle1 = document.querySelector("#circ1")
 const circle2 = document.querySelector("#circ2")
 
 let running = false
+let v1 = 0
+let v2 = 0
+let digits = 1
+let m = 100**digits
+let repetitions = 1
+let count = 0
 
 function start() {
     running = true
+    circle1.setAttribute("cx", 80)
+    circle2.setAttribute("cx", 20)
+    count = 0
+    v1 = -1
+    v2 = 0
     window.requestAnimationFrame(animationLoop)
 }
 
@@ -16,22 +26,40 @@ function animationLoop() {
     
     // Kommentiere hier (c)    
     // 
-    const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-    
+    let cx1 = parseFloat(circle1.getAttribute("cx"))     
+    let cx2 = parseFloat(circle2.getAttribute("cx"))     
+
+    // Start Loop
+    if (false) { // Aufgabe 4
+        ({v1, v2} = newVelocity(v1, v2))
+    }
+
     // Kommentiere hier (d)
     // 
-    let x = Math.random()
-    let y = Math.random()
-    dot.setAttribute("cx", x)
-    dot.setAttribute("cy", y)
-    dot.setAttribute("r", 0.01)
-    dot.setAttribute("fill", "black")
+    cx1 = cx1 + v1 / repetitions
+    cx2 = cx2 + v2 / repetitions
+
+    // End Loop
+    
     // Kommentiere hier (e)
-    svg.appendChild(dot)
+    circle1.setAttribute("cx", cx1)
+    circle2.setAttribute("cx", cx2)
 
     // Kommentiere hier (f)
     // 
     if ( running === true ) {
         window.requestAnimationFrame(animationLoop)
     }
+}
+
+
+function newVelocity(v1, v2) {
+    return {
+        v1: (m * v1 + (2 * v2 - v1)) / (m + 1),
+        v2: (v2 + m * (2 * v1 - v2)) / (m + 1)
+    }
+}
+
+function displayNumber() {
+    return (count / 10**digits).toFixed(digits)
 }
